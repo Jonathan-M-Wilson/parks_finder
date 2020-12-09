cb20714504cbd9eb9c13a7f750d71efce69acab6
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-09 11:17:02 -0700

    Rough draft controller with api calls to pass spec before refactor

---
+++ app/controllers/parks_controller.rb
@@ -0,0 +1,20 @@
+class ParksController < ApplicationController
+
+  def index
+    state_park = get_json("/api/v1/parks?q=#{params[:state]}")
+    @parks = state_park[:data]
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: 'https://developer.nps.gov') do |faraday|
+      faraday.params['api_key'] = ENV['NPS_API_KEY']
+    end
+  end
+
+  def get_json(url)
+    response = conn.get(url)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
