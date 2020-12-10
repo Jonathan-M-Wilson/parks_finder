7d13836a21f37d0f7a4bb919c3c595cb32525aab
Jonathan Wilson <JonathanMorrillWilson@gmail.com>
2020-12-10 14:59:49 -0700

    Remove refactored code

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,22 +1,6 @@
 class ParksController < ApplicationController
 
   def index
-    state_park = get_json("/api/v1/parks?q=#{params[:state]}")
-    @parks = state_park[:data].map do |park_data|
-      Park.new(park_data)
-    end
-  end
-
-  private
-
-  def conn
-    Faraday.new(url: 'https://developer.nps.gov') do |faraday|
-      faraday.params['api_key'] = ENV['NPS_API_KEY']
-    end
-  end
-
-  def get_json(url)
-    response = conn.get(url)
-    JSON.parse(response.body, symbolize_names: true)
+    @parks = ParkFacade.find_park(params[:state])
   end
 end
